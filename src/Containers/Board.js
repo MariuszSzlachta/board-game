a1cda2d7f8b83bd4344fe633d5e0dc9c0e060baf
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-02-17 21:07:27 +0100

    Create basic version of game logic

--- src/Containers/Board.js
+++ src/Containers/Board.js
@@ -5,18 +5,74 @@ class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      diceRoll: 0,
+      numberOfDiceRolls: 0,
       diceCurrentRoll: null,
       diceResults: [],
-      playerPosition: null,
+      playerPosition: 0,
       boardSize: 20,
       playerPrevPosition: null,
-      playerTargetPosition: null
+      playerTargetPosition: null,
+      specialFields: [12, 19]
     }
   }
+
   onDiceRollHandler = () => {
     return Math.floor(Math.random(6)+1);
   }
+
+  specialFieldChecker = targetField => {
+    const specialField = this.state.specialFields.filter(field => field === targetField);
+
+    if (specialField){
+      switch (specialField) {
+        case 19:
+          return 11;
+        case 12:
+          return -1;
+        default:
+          return specialField;
+      }
+    } else {
+      return targetField;
+    }
+  }
+
+  onFindPlayerTargetPosition = () => {
+    const playerPrevPosition = {...this.state.playerPrevPosition};
+    const boardSize = {...this.state.boardSize};
+    const diceResult = this.onDiceRollHandler();
+
+    let playerTargetPosition = playerPrevPosition + diceResult;
+
+    switch (true) {
+      case playerTargetPosition > boardSize:
+        playerTargetPosition = playerPrevPosition + ( playerTargetPosition - boardSize );
+        break;
+
+      case playerTargetPosition === boardSize:
+        playerTargetPosition = boardSize;
+        // foo winGame
+        break;
+
+      case playerTargetPosition < boardSize:
+        playerTargetPosition = playerPrevPosition + diceResult;
+        break;
+
+      default:
+      break;
+    }
+
+    playerTargetPosition = this.specialFieldChecker(playerTargetPosition);
+
+    if (playerTargetPosition === -1) {
+      this.onLoseGame();
+    }
+  }
+
+  onPlayerMove = () => {
+
+  }
+
 
   render() {
     return (
