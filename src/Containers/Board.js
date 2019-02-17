4b0289192ee17bf724650d4e4801eaa3382e9353
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-02-17 20:05:02 +0100

    Add game state v0.1

--- src/Containers/Board.js
+++ src/Containers/Board.js
@@ -2,7 +2,20 @@ import React, { Component } from 'react';
 
 class Board extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      diceRoll: 0,
+      diceCurrentRoll: null,
+      diceResults: [],
+      playerPosition: null,
+      boardSize: 20,
+      playerPrevPosition: null,
+      playerTargetPosition: null
+    }
+  }
   
+
   render() {
     return (
       <div className="Board">
