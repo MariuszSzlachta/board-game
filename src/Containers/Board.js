2fad2d47da19a60bca79dd6a95aaf08b5561ce0e
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-02-17 20:04:18 +0100

    Init project

--- src/Containers/Board.js
+++ src/Containers/Board.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
+class Board extends Component {
 
-class App extends Component {
+  
   render() {
     return (
-      <div className="App">
+      <div className="Board">
         
       </div>
     );
   }
 }
 
-export default App;
+export default Board;
