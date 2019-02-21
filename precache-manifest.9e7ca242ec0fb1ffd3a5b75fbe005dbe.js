3b1b3036103e33aea4d7d48b42f94ddea44ebed2
Mariusz Szlachta <mariusz.szlachta@yahoo.com>
2019-02-21 19:04:57 +0100

    Updates

---
+++ precache-manifest.9e7ca242ec0fb1ffd3a5b75fbe005dbe.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "869714502a9f89b8131ee75599dc0512",
+    "url": "/board-game/static/media/win.86971450.mp3"
+  },
+  {
+    "revision": "9e97fca32731e93ada0f192037724d9f",
+    "url": "/board-game/static/media/player.9e97fca3.svg"
+  },
+  {
+    "revision": "c0986a3825ebecba979ef705604eb031",
+    "url": "/board-game/static/media/lose.c0986a38.mp3"
+  },
+  {
+    "revision": "19a940f939908ed8a39da10ccbad79ee",
+    "url": "/board-game/static/media/dice.19a940f9.mp3"
+  },
+  {
+    "revision": "3fdb2c96f1c605891a12",
+    "url": "/board-game/static/js/runtime~main.3fdb2c96.js"
+  },
+  {
+    "revision": "1176898e6ebda72d094a",
+    "url": "/board-game/static/js/main.1176898e.chunk.js"
+  },
+  {
+    "revision": "c5f653146eeaea8c099b",
+    "url": "/board-game/static/js/2.c5f65314.chunk.js"
+  },
+  {
+    "revision": "1176898e6ebda72d094a",
+    "url": "/board-game/static/css/main.1c215b20.chunk.css"
+  },
+  {
+    "revision": "d9084641215851db33b5a77cf41fd745",
+    "url": "/board-game/index.html"
+  }
+];
